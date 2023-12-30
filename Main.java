84bbb078f3bcb6ea99c96b5ade913a96c80966bf
olvo <alexvolenko9315@gmail.com>
2023-12-30 22:42:19 +0100

    seminar

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Collections;
+
+import list.MyArrayList;
+import list.MyList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyList<Integer> list = new MyArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(10);
+        System.out.println(list.get(1));
+        list.delete(1);
+        System.out.println(list);
+        System.out.println(list.size());
     }
     
 }
